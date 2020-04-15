b36676efd1b8ff15dce88d8c952ceae82b78ae57
Wesley White <wesleyrwhite86@gmail.com>
2020-04-15 09:37:43 -1000

    Setting up project

---
+++ Logo/Logo/LogoView.swift
@@ -0,0 +1,47 @@
+//
+//  LogoView.swift
+//  Logo
+//
+//  Created by Wesley Ryan on 4/15/20.
+//  Copyright © 2020 Wesley Ryan. All rights reserved.
+//
+
+import UIKit
+
+class LogoView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = UIColor.clear
+        
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            
+            
+            let rectangle = CGRect(x: rect.minX, y: rect.minY, width: rect.width, height: (rect.height / 3) * 2)
+            let roundedPath = CGPath(roundedRect: rectangle, cornerWidth: 8.0, cornerHeight: 8.0, transform: nil)
+    
+            context.addPath(roundedPath)
+            context.setFillColor(UIColor.white.cgColor)
+            context.fillPath()
+            
+            
+        }
+        
+    }
+    
+    
+}
+
+
---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by Wesley Ryan on 4/15/20.
+//  Copyright © 2020 Wesley Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
