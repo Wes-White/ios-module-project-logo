df97394030eeed86a740ed3ee24f771721eb2d86
Wesley White <wesleyrwhite86@gmail.com>
2020-04-15 14:00:44 -1000

    MVP Completed

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -36,6 +36,50 @@ class LogoView: UIView {
                 context.fillPath()
                 
                 
+                
+                //MARK: Bottom Triangle
+                let leftCorner = CGPoint(x: rectangle.minX + 5, y: rectangle.maxY)
+                let rightCorner = CGPoint(x: rectangle.maxX - 5, y: rectangle.maxY)
+                let centerBottom = CGPoint(x: rect.midX + 2, y: rect.maxY - 2)
+                let centerBottomLeft = CGPoint(x: rect.midX - 10, y: rect.maxY - 10)
+                let centerBottomRight = CGPoint(x: rect.midX + 10, y: rect.maxY - 10)
+                context.setStrokeColor(UIColor.black.cgColor)
+                context.beginPath()
+                context.move(to: leftCorner)
+                context.addLine(to: centerBottomLeft)
+                context.addQuadCurve(to: centerBottomRight, control: centerBottom)
+                context.addLine(to: rightCorner)
+                context.addLine(to: leftCorner)
+                context.closePath()
+                context.setFillColor(UIColor.red.cgColor)
+                context.fillPath()
+                context.strokePath()
+
+                
+                
+                //MARK: LAMBDA LOGO
+                let center = CGPoint(x: rectangle.midX, y: rectangle.midY)
+                context.setStrokeColor(UIColor.white.cgColor)
+                context.beginPath()
+                context.move(to: center)
+                context.setLineWidth(5)
+                context.addLine(to: CGPoint(x: rectangle.midX - 50, y: rectangle.midY + 70))
+                let leftLogoCorner = CGPoint(x: rectangle.midX - 85, y: rectangle.midY + 70)
+                context.addLine(to: CGPoint(x: rectangle.midX - 75, y: rectangle.midY + 70))
+                context.addQuadCurve(to: CGPoint(x: rectangle.midX - 85, y: rectangle.midY + 70), control: leftLogoCorner)
+                context.addLine(to: CGPoint(x: rectangle.midX - 20, y: rectangle.midY - 30))
+                context.addLine(to: CGPoint(x: rectangle.midX + 20, y: rectangle.midY - 30))
+                let rightLogoCorner = CGPoint(x: rectangle.midX + 85, y: rectangle.midY + 70)
+                context.addLine(to: CGPoint(x: rectangle.midX + 85, y: rectangle.midY + 60))
+                context.addQuadCurve(to: CGPoint(x: rectangle.midX + 75, y: rectangle.midY + 70), control: rightLogoCorner)
+                context.addLine(to: CGPoint(x: rectangle.midX + 50, y: rectangle.midY + 70))
+                context.addLine(to: center)
+                context.setFillColor(UIColor.white.cgColor)
+                context.closePath()
+                context.fillPath()
+                context.strokePath()
+                
+                
             }
         }
 
